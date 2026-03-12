95c162bc2cd0703b9f28bff1655c06d4b0126207
aalla-pavani <pavaniaalla2005@gmail.com>
2026-03-12 19:38:24 +0530

    connect frontend to deployed backend

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
       setLoading(true);
       setResult(null);
 
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://securexsheild.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url }),
